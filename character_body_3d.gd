68cd35f0001fdf105324ac2c4709a816e52d3ca0
matteomiceli <omatteomiceli@gmail.com>
2025-04-26 14:08:16 -0700

    better dashing bean

--- character_body_3d.gd
+++ character_body_3d.gd
@@ -5,17 +5,40 @@ var speed = 4.0  # movement speed
 var jump_speed = 6.0  # determines jump height
 var mouse_sensitivity = 0.002  # turning speed
 
+var is_dashing = false
+var dash_force = 40
+const DASH_DECAY = 200
+var dash_velocity: Vector3
 
 func get_input():
 	var input = Input.get_vector("strafe_left", "strafe_right", "move_forward", "move_back")
-	if Input.is_action_pressed("dash"):		
-		velocity.x = input.x * speed * 2
-		velocity.z = input.y * speed * 2
-	else:
-		velocity.x = input.x * speed
-		velocity.z = input.y * speed
+	velocity.x = input.x * speed
+	velocity.z = input.y * speed
+
+	if Input.is_action_just_pressed("dash") and not is_dashing:
+		is_dashing = true
+		dash_velocity.x = input.x * dash_force
+		dash_velocity.z = input.y * dash_force
+
+	if Input.is_action_pressed("jump"):
+		jump()
 
 func _physics_process(delta):
-	velocity.y += -gravity * delta
 	get_input()
+	velocity.y += -gravity * delta
+	if is_dashing:
+		var decay_amount = DASH_DECAY * delta
+		if dash_velocity.length() <= decay_amount:
+			dash_velocity = Vector3.ZERO
+			is_dashing = false
+		else:
+			dash_velocity -= dash_velocity.normalized() * decay_amount
+
+	velocity.x = velocity.x + dash_velocity.x
+	velocity.z = velocity.z + dash_velocity.z
+
 	move_and_slide()
+
+
+func jump():
+	velocity.y = 3
