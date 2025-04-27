bf8298b749788f50fb79515aff83933066de0107
matteomiceli <omatteomiceli@gmail.com>
2025-04-27 12:30:33 -0700

    feat: player rotation

--- player-controller.gd
+++ player-controller.gd
@@ -1,26 +1,39 @@
 extends CharacterBody3D
 
-const SPEED = 6
-const JUMP_VELOCITY = 5
+const SPEED := 6
+const JUMP_VELOCITY := 5
 
 # Dash
-var is_dashing = false
+var is_dashing := false
 var dash_velocity: Vector3
 const DASH_FORCE = 40
 const DASH_DECAY = 200
 
-func apply_gravity(delta: float):
-	velocity += get_gravity() * delta
 
+func _physics_process(delta: float):
+	handle_movement(delta)
+	apply_gravity(delta)
+
+	move_and_slide()
+
+
+func apply_gravity(delta: float):
+	if not is_on_floor():
+		velocity += get_gravity() * delta
 
 func handle_movement(delta: float):
-	var input := Input.get_vector("strafe_left", "strafe_right", "move_forward", "move_back")
+	var input_dir := Input.get_vector("strafe_left", "strafe_right", "move_forward", "move_back")
 	handle_dash_decay(delta)
 
 	# TODO: this feels like it wants to be distilled down to an `apply_speed_modifiers` function that
 	# applies all speed modifiers on the player, not just the dash modifier
-	velocity.x = (input.x * SPEED) + dash_velocity.x
-	velocity.z = (input.y * SPEED) + dash_velocity.z
+	velocity.x = (input_dir.x * SPEED) + dash_velocity.x
+	velocity.z = (input_dir.y * SPEED) + dash_velocity.z
+
+ 	# Handle player rotation
+	var look_direction := Vector3(input_dir.x, 0, input_dir.y)
+	if not position.is_equal_approx(position - look_direction):
+		self.look_at(position + look_direction)
 
 	if Input.is_action_just_pressed("jump"):
 		if is_on_floor():
@@ -29,8 +42,8 @@ func handle_movement(delta: float):
 	if Input.is_action_just_pressed("dash"):
 		if not is_dashing:
 			is_dashing = true
-			dash_velocity.x = input.x * DASH_FORCE
-			dash_velocity.z = input.y * DASH_FORCE
+			dash_velocity.x = input_dir.x * DASH_FORCE
+			dash_velocity.z = input_dir.y * DASH_FORCE
 
 
 func handle_dash_decay(delta: float):
@@ -43,11 +56,3 @@ func handle_dash_decay(delta: float):
 	else:
 		dash_velocity = Vector3.ZERO
 		is_dashing = false
-
-
-func _physics_process(delta: float):
-	handle_movement(delta)
-	apply_gravity(delta)
-
-
-	move_and_slide()
