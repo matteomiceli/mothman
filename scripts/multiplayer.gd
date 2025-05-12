90bca79edc3a11c72f7e47697542b7afa7f7a5a3
matteomiceli <omatteomiceli@gmail.com>
2025-05-11 23:47:02 -0700

    refactor: multiplayer code is all in one place

--- scripts/multiplayer.gd
+++ scripts/multiplayer.gd
@@ -1,18 +1,33 @@
 extends Node
 
+@onready var host_color_picker = $ServerMenu/ItemList/HostColorPicker
+@onready var client_color_picker = $ServerMenu/ItemList/ClientColorPicker
+@onready var world = $World
+
 const PORT = 4433
 
 func _ready() -> void:
-	#Global.countdown_finished.connect(_on_countdown_finished)
-	
 	# Pause game
 	get_tree().paused = true
+
+	start_server()
+	register_listeners()
+	#Global.countdown_finished.connect(_on_countdown_finished)
+
+func start_server(): 
 	multiplayer.server_relay = false
 
 	# Start the server in headless mode.
 	if DisplayServer.get_name() == "headless":
 		print("Automatically starting dedicated server.")
 		_on_host_pressed.call_deferred()
+
+func register_listeners():
+	if not multiplayer.is_server(): return
+
+	multiplayer.peer_connected.connect(world.add_player)
+	multiplayer.peer_disconnected.connect(world.remove_player)
+
 
 func _on_host_pressed():
 	# Start host
@@ -22,9 +37,9 @@ func _on_host_pressed():
 		OS.alert("Failed to host game.")
 		return
 	multiplayer.multiplayer_peer = peer
-	
-	# Add server host player to game
-	$World.add_player(1)
+
+	# Add server host player
+	world.add_player(multiplayer.get_unique_id())
 	start_game()
 	
 func _on_client_pressed():
@@ -49,3 +64,11 @@ func _on_client_pressed():
 func start_game():
 	$ServerMenu.hide()
 	get_tree().paused = false
+
+
+func _exit_tree() -> void:
+	# Cleanup listeners
+	if not multiplayer.is_server(): return 
+
+	multiplayer.peer_connected.disconnect(world.add_player)
+	multiplayer.peer_disconnected.disconnect(world.remove_player)
--- scripts/world.gd
+++ scripts/world.gd
@@ -1,24 +1,18 @@
 extends Node3D
 
+@onready var player_spawner = $PlayersSpawn
+
+var Player = preload("res://scenes/player.tscn")
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	if Global.game_mode == Global.MODE.SINGLEPLAYER:
+		pass
 		add_player(1)
 
-	if multiplayer.is_server():
-		setup_multiplayer()
-
-func setup_multiplayer():
-	# Listen for peers connecting and disconnecting
-	multiplayer.peer_connected.connect(add_player)
-	multiplayer.peer_disconnected.connect(remove_player)
-
-	# Handle already connected peers
-	for id in multiplayer.get_peers():
-		add_player(id)
-
 func add_player(id: int):
-	var player = preload("res://scenes/player.tscn").instantiate()
+	var player = Player.instantiate()
+	print(player)
 	player.name = str(id)
 
 	if len(multiplayer.get_peers()) > 0:
@@ -26,15 +20,9 @@ func add_player(id: int):
 		# https://forum.godotengine.org/t/setting-position-on-spawn-in-multiplayer-causes-client-to-spawn-at-0-0-0/78584/7
 		player.position.x += 10
 	
-	$PlayersSpawn.add_child(player, true)
+	player_spawner.add_child(player, true)
 
 func remove_player(id: int):
 	print("remove", id)
-	if $PlayersSpawn.has_node(str(id)):
-		$PlayersSpawn.get_node(str(id)).queue_free()
-
-func _exit_tree() -> void:
-	# Cleanup listeners
-	if multiplayer.is_server():
-		multiplayer.peer_connected.disconnect(add_player)
-		multiplayer.peer_disconnected.disconnect(remove_player)
+	if player_spawner.has_node(str(id)):
+		player_spawner.get_node(str(id)).queue_free()
