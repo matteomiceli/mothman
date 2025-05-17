370a7655bb0cf1fb60bc6b910fe96a035e0f0330
Adam Tuck <tuckaet@gmail.com>
2025-05-17 13:16:44 -0700

    cleanup steam-lobby script

--- scripts/global.gd
+++ scripts/global.gd
@@ -7,6 +7,7 @@ var player: Node3D = null
 enum MODE {MULTIPLAYER, SINGLEPLAYER, NULL}
 var game_mode: MODE = MODE.NULL
 
+var APP_ID = 480 # Spacewar, dev app id
 var OWNED = false
 var ONLINE = false
 var STEAM_ID = 0
@@ -17,21 +18,23 @@ var LOBBY_MEMBERS = []
 var LOBBY_INVITE_ARG = false
 var MAX_LOBBY_PLAYERS = 8
 
+func _init():
+	OS.set_environment("SteamAppId", str(APP_ID))
+	OS.set_environment("SteamGameId", str(APP_ID))
+
 func _ready():
-	var init: Dictionary = Steam.steamInitEx(480)
+	var init: Dictionary = Steam.steamInitEx(APP_ID)
 	if init['status'] != 0:
 		print("Failed to instantiate Steam: %s. Shutting down..." % init['verbal'])
 		get_tree().quit()
 		
+	if not Steam.isSubscribed():
+		print("Person does not own this game.")
+		get_tree().quit()
+	
 	ONLINE = Steam.loggedOn()
 	STEAM_ID = Steam.getSteamID()
 	STEAM_NAME = Steam.getPersonaName()
-	OWNED = Steam.isSubscribed()
-	
-	if OWNED == false:
-		print("Person does not own this game.")
-		get_tree().quit()
-		
 	print("Steam initialized.\n online: %s, steam_id: %s, steam_name: %s" % [ONLINE, STEAM_ID, STEAM_NAME])
 
 func _process(delta):
--- scripts/steam-lobby.gd
+++ scripts/steam-lobby.gd
@@ -1,21 +1,23 @@
 extends Node2D
 
-enum lobby_status {Private, Friends, Public, Invisible}
-enum search_distance {Close, Default, Far, Worldwide}
+enum LobbyStatus { Private, Friends, Public, Invisible }
+enum SearchDistance { Close, Default, Far, Worldwide }
 
-@onready var steamName = $SteamName
-@onready var lobbySetName = $CreateButton/TextEdit
-@onready var chatInput = $SendButton/TextEdit
-@onready var lobbyGetName = $Chat/Label
-@onready var lobbyOutput = $Chat/RichTextLabel
-@onready var playerCount = $Players/Label
-@onready var playerList = $Players/RichTextLabel
-@onready var lobbyPopup = $Popup
-@onready var lobbyList = $Popup/Panel/Scroll/VBox
+@onready var steam_name_label = $SteamName
+@onready var lobby_name_input = $CreateButton/TextEdit
+@onready var chat_input = $SendButton/TextEdit
+@onready var lobby_name_label = $Chat/Label
+@onready var chat_output = $Chat/RichTextLabel
+@onready var player_count_label = $Players/Label
+@onready var player_list_output = $Players/RichTextLabel
+@onready var lobby_popup = $Popup
+@onready var lobby_list_container = $Popup/Panel/Scroll/VBox
 
 func _ready():
-	print("Steam overlay enabled: ", Steam.isOverlayEnabled())
-	steamName.text = Global.STEAM_NAME
+	print("Steam overlay enabled:", Steam.isOverlayEnabled())
+	steam_name_label.text = Global.STEAM_NAME
+
+	# Connect Steam signals
 	Steam.connect("lobby_created", Callable(self, "_on_lobby_created"))
 	Steam.connect("lobby_match_list", Callable(self, "_on_lobby_match_list"))
 	Steam.connect("lobby_joined", Callable(self, "_on_lobby_joined"))
@@ -23,150 +25,160 @@ func _ready():
 	Steam.connect("lobby_message", Callable(self, "_on_lobby_message"))
 	Steam.connect("lobby_data_update", Callable(self, "_on_lobby_data_update"))
 	Steam.connect("join_requested", Callable(self, "_on_lobby_join_requested"))
+
 	check_command_line()
 
 func create_lobby():
 	if Global.LOBBY_ID == 0:
-		Steam.createLobby(lobby_status.Public, Global.MAX_LOBBY_PLAYERS)
+		Steam.createLobby(LobbyStatus.Public, Global.MAX_LOBBY_PLAYERS)
 
-func join_lobby(lobbyId):
-	print('join lobby called with lobbyId %s' % lobbyId)
-	lobbyPopup.hide()
-	var name = Steam.getLobbyData(lobbyId, "name")
-	display_message("Joining lobby \"%s\"..." % name)
-	
+func join_lobby(lobby_id):
+	print("Joining lobby:", lobby_id)
+	lobby_popup.hide()
+
+	var lobby_name = Steam.getLobbyData(lobby_id, "name")
+	display_message("Joining lobby \"%s\"..." % lobby_name)
+
 	Global.LOBBY_MEMBERS.clear()
-	Global.LOBBY_ID = lobbyId
-	
-	Steam.joinLobby(lobbyId)
+	Global.LOBBY_ID = lobby_id
+
+	Steam.joinLobby(lobby_id)
 	set_lobby_members()
 
 func set_lobby_members():
 	Global.LOBBY_MEMBERS.clear()
-	var memberCount = Steam.getNumLobbyMembers(Global.LOBBY_ID)
-	playerCount.set_text("Players (%s)" % str(memberCount))
-	print('membercount'+str(memberCount))
-	for member in range(0, memberCount):
-		print('lobid%s'%Global.LOBBY_ID)
-		var MEMBER_STEAM_ID = Steam.getLobbyMemberByIndex(Global.LOBBY_ID, member)
-		var MEMBER_STEAM_NAME = Steam.getFriendPersonaName(MEMBER_STEAM_ID)
-		print("<|%s, %s|>" % [MEMBER_STEAM_ID, MEMBER_STEAM_NAME])
-		add_player_list(MEMBER_STEAM_ID, MEMBER_STEAM_NAME) 
-	
-func add_player_list(steamId, steamName):
-	print("add playing %s %s" % [steamId, steamName])
-	Global.LOBBY_MEMBERS.append({"steam-id":steamId, "steam-name":steamName})
-	playerList.clear()
+
+	var count = Steam.getNumLobbyMembers(Global.LOBBY_ID)
+	player_count_label.text = "Players (%d)" % count
+	print("Member count:", count)
+
+	for i in count:
+		var id = Steam.getLobbyMemberByIndex(Global.LOBBY_ID, i)
+		var name = Steam.getFriendPersonaName(id)
+		print("Member: %s (%s)" % [name, id])
+		Global.LOBBY_MEMBERS.append({ "steam-id": id, "steam-name": name })
+
+	update_player_list()
+
+func update_player_list():
+	player_list_output.clear()
 	for member in Global.LOBBY_MEMBERS:
-		playerList.add_text("%s\n" % str(member['steam-name']))
+		player_list_output.add_text("%s\n" % member["steam-name"])
 
 func send_chat_message():
-	var message = chatInput.text
-	var sent = Steam.sendLobbyChatMsg(Global.LOBBY_ID, message)
-	if not sent:
-		display_message("ERROR: Chat message failed to send.")
-	chatInput.text = ""
+	var message = chat_input.text
+	if message.is_empty():
+		return
+
+	if !Steam.sendLobbyChatMsg(Global.LOBBY_ID, message):
+		display_message("Failed to send message.")
+
+	chat_input.text = ""
 
 func display_message(message):
-	lobbyOutput.add_text("\n%s" % str(message))
+	chat_output.add_text("\n%s" % message)
 
-func _on_lobby_created(connect, lobbyId):
-	if (connect == 1):
-		Global.LOBBY_ID = lobbyId
-		display_message("Created Lobby: %s" % lobbySetName.text)
-		print(lobbyId)
-		
-	Steam.setLobbyData(lobbyId, "name", lobbySetName.text)
-	var name = Steam.getLobbyData(lobbyId, "name")
-	lobbyGetName.text = str(name)
-
-func _on_lobby_match_list(lobbyIds):
-	for lobbyId in lobbyIds:
-		var lobbyName = Steam.getLobbyData(lobbyId, "name")
-		var lobbyMembers = Steam.getNumLobbyMembers(lobbyId)
-		var LOBBY_BUTTON = Button.new()
-		LOBBY_BUTTON.set_text("Lobby %s: %s, [%d] Player(s)" % [lobbyId, lobbyName, lobbyMembers])
-		LOBBY_BUTTON.set_size(Vector2(800,50))
-		LOBBY_BUTTON.set_name("lobby_%s" % str(lobbyId))
-		LOBBY_BUTTON.connect("pressed", Callable(self, "join_lobby").bind(lobbyId))
-		
-		lobbyList.add_child(LOBBY_BUTTON)
-		
-func _on_lobby_joined(lobbyId, permissions, locked, response):
-	Global.LOBBY_ID = lobbyId
-	var name = Steam.getLobbyData(lobbyId, "name")
-	lobbyGetName.text = str(name)
-	print('calling')
-	set_lobby_members()
-
-func _on_lobby_join_requested(lobbyId, friendId):
-	display_message("Joining %s\'s lobby" % Steam.getFriendPersonaName(friendId))
-	join_lobby(lobbyId)
-
-func _on_lobby_data_update(success, lobbyId, memberId, key):
-	print("Success: %s, LobbyId: %s, MemberId: %s, Key %s" % [success, lobbyId, memberId, key])
-
-func _on_lobby_chat_update(lobbyId, changedId, makingChangeId, chatState):
-	var changer = Steam.getFriendPersonaName(changedId)
-	if chatState == 1:
-		display_message("%s has joined the lobby" % changer)
-	elif chatState == 2:
-		display_message("%s has left the lobby" % changer)
-	elif chatState == 8:
-		display_message("%s has been kicked from the lobby" % changer)
-	elif chatState == 16:
-		display_message("%s has been banned from the lobby" % changer)
+func _on_lobby_created(success, lobby_id):
+	if success == 1:
+		Global.LOBBY_ID = lobby_id
+		var lobby_name = lobby_name_input.text
+		display_message("Created Lobby: %s" % lobby_name)
+		Steam.setLobbyData(lobby_id, "name", lobby_name)
+		lobby_name_label.text = lobby_name
 	else:
-		display_message("%s did something unknown" % changer)
+		display_message("Failed to create lobby.")
+
+func _on_lobby_match_list(lobby_ids):
+	for child in lobby_list_container.get_children():
+		child.queue_free()
+
+	for lobby_id in lobby_ids:
+		var name = Steam.getLobbyData(lobby_id, "name")
+		var count = Steam.getNumLobbyMembers(lobby_id)
 		
+		var btn = Button.new()
+		btn.text = "Lobby %s: %s (%d players)" % [lobby_id, name, count]
+		btn.size = Vector2(800, 50)
+		btn.name = "lobby_%s" % str(lobby_id)
+		btn.connect("pressed", Callable(self, "join_lobby").bind(lobby_id))
+
+		lobby_list_container.add_child(btn)
+
+func _on_lobby_joined(lobby_id, _perm, _locked, _response):
+	Global.LOBBY_ID = lobby_id
+	var name = Steam.getLobbyData(lobby_id, "name")
+	lobby_name_label.text = name
+	print("Joined lobby:", name)
 	set_lobby_members()
 
-func _on_lobby_message(result, user, message, type):
-	var sender = Steam.getFriendPersonaName(user)
-	display_message("%s: %s" % [sender, message])
-	
+func _on_lobby_join_requested(lobby_id, friend_id):
+	var friend_name = Steam.getFriendPersonaName(friend_id)
+	display_message("%s invited you to a lobby." % friend_name)
+	join_lobby(lobby_id)
+
+func _on_lobby_data_update(success, lobby_id, member_id, key):
+	print("Lobby data updated | Success: %s | Lobby: %s | Member: %s | Key: %s" % [success, lobby_id, member_id, key])
+
+func _on_lobby_chat_update(_lobby_id, changed_id, _making_change_id, chat_state):
+	var name = Steam.getFriendPersonaName(changed_id)
+
+	match chat_state:
+		1: display_message("%s joined the lobby." % name)
+		2: display_message("%s left the lobby." % name)
+		8: display_message("%s was kicked." % name)
+		16: display_message("%s was banned." % name)
+		_: display_message("Unknown change by %s" % name)
+
+	set_lobby_members()
+
+func _on_lobby_message(_result, user_id, message, _type):
+	var name = Steam.getFriendPersonaName(user_id)
+	display_message("%s: %s" % [name, message])
+
 func leave_lobby():
-	if Global.LOBBY_ID != 0:
-		display_message('Leaving lobby...')
-		Steam.leaveLobby(Global.LOBBY_ID)
-		Global.LOBBY_ID = 0
-		
-		lobbyGetName.text = "Lobby"
-		playerCount.text = "Players (0)"
-		playerList.clear()
-		
-		for member in Global.LOBBY_MEMBERS:
-			Steam.closeP2PSessionWithUser(member['steam-id'])
-			
-		Global.LOBBY_MEMBERS.clear()
-		
-func _on_start_button_pressed() -> void:
+	if Global.LOBBY_ID == 0:
+		return
+
+	display_message("Leaving lobby...")
+	Steam.leaveLobby(Global.LOBBY_ID)
+	Global.LOBBY_ID = 0
+
+	lobby_name_label.text = "Lobby"
+	player_count_label.text = "Players (0)"
+	player_list_output.clear()
+
+	for member in Global.LOBBY_MEMBERS:
+		Steam.closeP2PSessionWithUser(member["steam-id"])
+
+	Global.LOBBY_MEMBERS.clear()
+
+# --- UI Events ---
+
+func _on_start_button_pressed():
 	get_tree().change_scene_to_file("res://scenes/game.tscn")
 
-func _on_create_button_pressed() -> void:
+func _on_create_button_pressed():
 	create_lobby()
 
-func _on_join_button_pressed() -> void:
-	lobbyPopup.popup()
-	Steam.addRequestLobbyListDistanceFilter(search_distance.Worldwide)
-	display_message('Searching for lobbies...')
+func _on_join_button_pressed():
+	lobby_popup.popup()
+	Steam.addRequestLobbyListDistanceFilter(SearchDistance.Worldwide)
+	display_message("Searching for lobbies...")
 	Steam.requestLobbyList()
 
-func _on_leave_button_pressed() -> void:
+func _on_leave_button_pressed():
 	leave_lobby()
 
-func _on_send_button_pressed() -> void:
+func _on_send_button_pressed():
 	send_chat_message()
 
-func _on_close_pressed() -> void:
-	lobbyPopup.hide()
+func _on_close_pressed():
+	lobby_popup.hide()
 
-# For accepting steam invites out of process
+# Enables accepting steam invites out of process
 func check_command_line():
-	var ARGS = OS.get_cmdline_args()
-	if ARGS.size() > 0:
-		for arg in ARGS:
-			if Global.LOBBY_INVITE_ARG:
-				join_lobby(int(arg))
-			if arg == "+connect_lobby":
-				Global.LOBBY_INVITE_ARG = true
+	for arg in OS.get_cmdline_args():
+		if Global.LOBBY_INVITE_ARG:
+			join_lobby(int(arg))
+		elif arg == "+connect_lobby":
+			Global.LOBBY_INVITE_ARG = true
