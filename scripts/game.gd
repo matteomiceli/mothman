56ca5e70d69cc3122bc7447f6e4b16f02be49b1a
matteomiceli <omatteomiceli@gmail.com>
2025-05-24 10:21:10 -0700

    refactor: move steam logic out of global
    This allows us to switch between steam and Enet multiplayer without
    needing steam as a development dependency.

--- scripts/game.gd
+++ scripts/game.gd
@@ -1,5 +1,18 @@
 extends Node3D
 
+func _init():
+	if Global.game_mode == Global.MODE.STEAM_MULTIPLAYER:
+		OS.set_environment("SteamAppId", str(Global.APP_ID))
+		OS.set_environment("SteamGameId", str(Global.APP_ID))
+
+func _ready() -> void:
+	if Global.game_mode == Global.MODE.STEAM_MULTIPLAYER:
+		init_steam()
+
+func _process(_delta: float) -> void:
+	if Global.game_mode == Global.MODE.STEAM_MULTIPLAYER:
+		Steam.run_callbacks()
+
 func _on_singleplayer_pressed():
 	Global.game_mode = Global.MODE.SINGLEPLAYER
 	$Mode.add_child(preload("res://scenes/singleplayer.tscn").instantiate())
@@ -10,3 +23,19 @@ func _on_multiplayer_pressed():
 	Global.game_mode = Global.MODE.MULTIPLAYER
 	$Mode.add_child(preload("res://scenes/multiplayer.tscn").instantiate())
 	$Menu.hide()
+
+
+func init_steam() ->  void:
+	var init: Dictionary = Steam.steamInitEx(Global.APP_ID)
+	if init['status'] != 0:
+		print("Failed to instantiate Steam: %s. Shutting down..." % init['verbal'])
+		get_tree().quit()
+		
+	if not Steam.isSubscribed():
+		print("Person does not own this game.")
+		get_tree().quit()
+	
+	Global.ONLINE = Steam.loggedOn()
+	Global.STEAM_ID = Steam.getSteamID()
+	Global.STEAM_NAME = Steam.getPersonaName()
+	print("Steam initialized.\n online: %s, steam_id: %s, steam_name: %s" % [Global.ONLINE, Global.STEAM_ID, Global.STEAM_NAME])
--- scripts/global.gd
+++ scripts/global.gd
@@ -7,6 +7,7 @@ var player: Node3D = null
 enum MODE {MULTIPLAYER, SINGLEPLAYER, STEAM_MULTIPLAYER, NULL}
 var game_mode: MODE = MODE.NULL
 
+# Steam
 var APP_ID = 480 # Spacewar, dev app id
 var OWNED = false
 var ONLINE = false
@@ -17,30 +18,3 @@ var LOBBY_ID = 0
 var LOBBY_MEMBERS = []
 var LOBBY_INVITE_ARG = false
 var MAX_LOBBY_PLAYERS = 8
-
-func _init():
-	OS.set_environment("SteamAppId", str(APP_ID))
-	OS.set_environment("SteamGameId", str(APP_ID))
-
-func _ready():
-	if game_mode == MODE.STEAM_MULTIPLAYER:
-		initialize_steam()
-
-func _process(delta: float):
-	if game_mode == MODE.STEAM_MULTIPLAYER:
-		Steam.run_callbacks()
-
-func initialize_steam() ->  void:
-	var init: Dictionary = Steam.steamInitEx(APP_ID)
-	if init['status'] != 0:
-		print("Failed to instantiate Steam: %s. Shutting down..." % init['verbal'])
-		get_tree().quit()
-		
-	if not Steam.isSubscribed():
-		print("Person does not own this game.")
-		get_tree().quit()
-	
-	ONLINE = Steam.loggedOn()
-	STEAM_ID = Steam.getSteamID()
-	STEAM_NAME = Steam.getPersonaName()
-	print("Steam initialized.\n online: %s, steam_id: %s, steam_name: %s" % [ONLINE, STEAM_ID, STEAM_NAME])
